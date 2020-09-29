093b4d9a37a70a2fbc3a64cc8902f9f963ffbcd0
OD-Ice <493965799@qq.com>
2020-09-29 12:20:41 +0800

    lower database name

--- AcademyCity/settings.py
+++ AcademyCity/settings.py
@@ -92,7 +92,7 @@ CACHES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'AcademyCity',
+        'NAME': 'academycity',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'USER': 'root',
