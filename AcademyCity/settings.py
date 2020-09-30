a228bf3bee12192f4e02dedd1b6a1c159042c4c8
OD-Ice <493965799@qq.com>
2020-09-30 15:26:25 +0800

    media settings

--- AcademyCity/settings.py
+++ AcademyCity/settings.py
@@ -69,6 +69,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # 媒体文件
+                'django.template.context_processors.media',
             ],
             'builtins': [
                 'django.templatetags.static'
