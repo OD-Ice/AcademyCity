ed5da5689b11e51da22bcebd5624a6c5226f3611
OD-Ice <493965799@qq.com>
2020-10-04 10:38:17 +0800

    wsgi

--- academycity/wsgi.py
+++ academycity/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AcademyCity.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'academycity.settings')
 
 application = get_wsgi_application()
